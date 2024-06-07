a2a6bb5ca0adfe99350dd28c9046fe3fdeda50a7
praniya99 <praneethravishka@gmail.com>
2024-06-07 22:40:57 +0530

    dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const UserRoutes = require('./Router/user.router.js');
 const DeliveryRoutes = require('./Router/deliver.router.js');
 const BeauticiansRoutes = require('./Router/beautician.router.js');
@@ -13,7 +17,7 @@ const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 
-app.use('/user', UserRoutes); // Use the user routes
+app.use('/user', UserRoutes); 
 app.use('/deliver', DeliveryRoutes); 
 app.use('/beautician', BeauticiansRoutes); 
 app.use('/comment', CommentRoutes); 
@@ -21,14 +25,11 @@ app.use('/checkout',CheckoutRoutes );
 app.use('/additem', AdditemRoutes); 
 
 
-
-mongoose.connect("mongodb+srv://praneeth:grp123@cluster.jxwk423.mongodb.net/?retryWrites=true&w=majority&appName=Cluster")
-
-
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log("connected to db");
-        app.listen(3000, () => {
-            console.log("server is running on port 3000");
+        app.listen(process.env.PORT, () => {
+            console.log(`server is running on port ${process.env.PORT}`);
         });
     })
     .catch((err) => {
